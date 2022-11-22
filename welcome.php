c422bc50bf7bc7c752955ae08aae28d2e42b8959
khudzu <64119084+khudzu@users.noreply.github.com>
2022-11-22 12:49:18 +0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -12,6 +12,21 @@ $firstname=$_POST["firstname"];
 $lastname=$_POST["lastname"];
 $email=$_POST["email"];
 // Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
